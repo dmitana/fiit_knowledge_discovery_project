9994d86027d53e428cdb008d18d2396e7cbf1ded
Denis Mitana <denis.mitana1@gmail.com>
2020-03-21 14:53:47 +0100

    Add preprocessing pipelines of weather data

--- src/data/pipelines.py
+++ src/data/pipelines.py
@@ -1,5 +1,5 @@
 from src.data.preprocessing import PrimaryUseTransformer, \
-    FeatureSelectorTransformer
+    FeatureSelectorTransformer, RollingAverageNanTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
@@ -11,5 +11,35 @@ primary_use_pipeline = Pipeline([
 
 square_feet_pipeline = Pipeline([
     ('feature select', FeatureSelectorTransformer('square_feet')),
+    ('normalization', StandardScaler())
+])
+
+air_temperature_pipeline = Pipeline([
+    ('rolling average', RollingAverageNanTransformer('air_temperature')),
+    ('feature select', FeatureSelectorTransformer('air_temperature')),
+    ('normalization', StandardScaler())
+])
+
+dew_temperature_pipeline = Pipeline([
+    ('rolling average', RollingAverageNanTransformer('dew_temperature')),
+    ('feature select', FeatureSelectorTransformer('dew_temperature')),
+    ('normalization', StandardScaler())
+])
+
+sea_level_pressure_pipeline = Pipeline([
+    ('rolling average', RollingAverageNanTransformer('sea_level_pressure')),
+    ('feature select', FeatureSelectorTransformer('sea_level_pressure')),
+    ('normalization', StandardScaler())
+])
+
+wind_speed_pipeline = Pipeline([
+    ('rolling average', RollingAverageNanTransformer('wind_speed')),
+    ('feature select', FeatureSelectorTransformer('wind_speed')),
+    ('normalization', StandardScaler())
+])
+
+wind_direction_pipeline = Pipeline([
+    ('rolling average', RollingAverageNanTransformer('wind_direction')),
+    ('feature select', FeatureSelectorTransformer('wind_direction')),
     ('normalization', StandardScaler())
 ])
